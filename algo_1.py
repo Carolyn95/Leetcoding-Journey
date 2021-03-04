482eafe2d83db97348329e85da251d3c41d4be3c
Carolyn Chen <carolynyanchongchen@gmail.com>
2021-03-04 20:35:27 +0800

    leetcode twosum

--- algo_1.py
+++ algo_1.py
@@ -1,108 +1,30 @@
-# 1. A、B、C、D、E五人在某天夜里合伙捕鱼 最后疲惫不堪各自睡觉
-# 2. 第二天A第一个醒来 他将鱼分为5份 扔掉多余的1条 拿走自己的一份
-# 3. B第二个醒来 也将鱼分为5份 扔掉多余的1条 拿走自己的一份
-# 4. 然后C、D、E依次醒来也按同样的方式分鱼 问他们至少捕了多少条鱼
+# Two sum
+# Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+class Solution1:
 
-{A: a, B: b, C: c, D: d, E: e}
-
-total = 5 * a + 1
-4 * a = 5 * b + 1
-4 * b = 5 * c + 1
-4 * c = 5 * d + 1
-4 * d = 5 * e + 1 
+  def twoSum(self, nums, target):
+    for i in range(len(nums)):
+      for j in range(i + 1, len(nums)):
+        if nums[i] + nums[j] == target:
+          return [i, j]
 
 
-(d1 - 1) % 5 == 0
-d1 = 6  = 6 + 5 * 0
-d1 = 11 = 6 + 5 * 1
-d1 = 16 = 6 + 5 * 2
-d1 = 21 = 6 + 5 * 3
+# Optimised using hash map
+class Solution:
+
+  def twoSum(self, nums, target):
+    hash_map = {}
+    for i, n in enumerate(nums):
+      remaining = target - n
+      if remaining in hash_map:
+        return [i, hash_map[remaining]]
+      else:
+        hash_map[n] = i
 
 
-
-
-
-
-# solution
-fish = 6
-while True:
-    total = fish
-    enough = True
-    t = []
-    for _ in range(5):
-        t.append((total - 1) / 5)
-        if (total - 1) % 5 == 0:
-            total = (total - 1) / 5 * 4    
-        else:
-            enough = False
-            break
-    if enough:
-        print(t)
-        print(fish)
-        break
-    fish += 5
-    
-fish = 6
-attempt = 1
-while True:
-    total = fish
-    enough = True
-    t = []
-    for _ in range(5):
-        t.append(total)
-        if (total - 1) % 5 == 0:
-            total = (total - 1) // 5 * 4
-        else:
-            enough = False
-            break
-    if enough:
-        if attempt != 2:
-            enough = False
-            attempt +=1
-        else:
-            print(t)
-            print(fish)
-            break 
-    fish += 5
-
- fish = 6
- while True:
-     total = fish
-     enough = True
-     attempt = 1
-     t = []
-     for _ in range(5):
-         t.append((total - 1) / 5)
-         if (total - 1) % 5 == 0:
-             total = (total - 1) / 5 * 4    
-         else:
-             enough = False
-             break
-     if enough:
-         if attempt == 2:
-             print(t)
-             print(fish)
-             break
-         else: 
-             enough = False
-             attempt += 1
-             continue
-     fish += 5
-
-# 1
-4 * (c + d + b + a) - 4 = 5 * (e + d + c + b)
-# 2
-total = 5 * a + 1
-a = (5 * b + 1) / 4
-b = (5 * c + 1) / 4
-c = (5 * d + 1) / 4
-d = (5 * e + 1) / 4
-
-total = 5 * ((5 * ((5 * ((5 * ((5 * e + 1) / 4) + 1) / 4) + 1) / 4) + 1) / 4) + 1
-
-((5 * ((5 * e + 1) / 4) + 1) / 4) + 1) / 4)
-4 
-(x + 1) % 4 == 0
-7
-
-
+if __name__ == '__main__':
+  nums = [2, 7, 11, 15]
+  target = 9
+  sol = Solution()
+  res = sol.twoSum(nums, target)
+  print(res)
