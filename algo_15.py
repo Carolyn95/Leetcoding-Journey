b84b13e57a1dbbf3b22f569ea7efeb0794a760ab
Carolyn Chen <carolynyanchongchen@gmail.com>
2021-03-07 13:35:07 +0800

    2 solutions with optimization

--- algo_15.py
+++ algo_15.py
@@ -3,9 +3,10 @@
 # Notice that the solution set must not contain duplicate triplets.
 
 
-class Solution:
+class Solution1:
 
-  def threeSum(self, nums: List[int]) -> List[List[int]]:
+  # brute force
+  def threeSum(self, nums):
     nums.sort()
     N = len(nums)
     res = []
@@ -17,6 +18,77 @@ class Solution:
     real_res = [r for i, r in enumerate(res) if r not in res[i + 1:]]
 
     return real_res
+
+
+class Solution2:
+  # optimized brute force
+  def threeSum(self, nums):
+    nums.sort()
+    N = len(nums)
+    res = []
+    for i in range(N):
+      for j in range(i + 1, N):
+        for k in range(j + 1, N):
+          ans = [nums[i], nums[j], nums[k]]
+          if sum(ans) == 0 and ans not in res:  # optimize solution1 here
+            res.append(ans)
+
+    return res
+
+
+class Solution3:
+  # double pointer
+  def threeSum(self, nums):
+    res = []
+    N = len(nums)
+    if N < 3:
+      return res
+
+    nums.sort()
+    for i in range(N - 2):
+      j = i + 1
+      k = N - 1
+      while j < k:
+        temp = [nums[i], nums[j], nums[k]]
+        added = sum(temp)
+        if added > 0:
+          k -= 1
+        else:
+          j += 1
+        if temp not in res and added == 0:
+          res.append(temp)
+
+    return res
+
+
+class Solution:
+  # optimized double pointer
+  def threeSum(self, nums):
+    res = []
+    N = len(nums)
+    if N < 3:
+      return res
+
+    nums.sort()
+    for i in range(N - 2):
+      j = i + 1
+      k = N - 1
+      while j < k:
+        temp = [nums[i], nums[j], nums[k]]
+        added = sum(temp)
+        if added > 0:
+          k -= 1
+        elif added < 0:
+          j += 1
+        else:
+          j += 1
+          if res:
+            if temp not in res:
+              res.append(temp)
+          else:
+            res.append(temp)
+
+    return res
 
 
 if __name__ == '__main__':
